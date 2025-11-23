5e11e39daf5aa97b37024157f6c2a9076cad0ab6
RominaRodriguez5 <mosqueiraromina@gmail.com>
2025-11-23 21:34:15 +0100

    Añadidas dependencias de Jackson en el pom.xml e incorporadas las clases ApiClient, Media y Usuari dentro del proyecto

---
+++ src/main/java/mosqueira/pureStream/Modelo/Media.java
@@ -0,0 +1,30 @@
+package mosqueira.pureStream.Modelo;
+/**
+ *
+ * @author Lulas
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// Created by Github Copilot
+
+public class Media {
+    @JsonProperty("id")
+    public int id;
+    @JsonProperty("userId")
+    public int userId;
+    @JsonProperty("downloadedFromUrl")
+    public String downloadedFromUrl;
+    @JsonProperty("mediaFileName")
+    public String mediaFileName;
+    @JsonProperty("mediaMimeType")
+    public String mediaMimeType;
+    @JsonProperty("blobNameGuid")
+    public String blobNameGuid;
+    @JsonProperty("blobUrl")
+    public String blobUrl;
+
+    @Override
+    public String toString() {
+        return String.format("Media{id=%d, userId=%d, file=%s, blob=%s}", id, userId, mediaFileName, blobNameGuid);
+    }
+}
